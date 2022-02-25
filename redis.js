9494bcfe0a3a9c6a7ee1650464f2fe1f82ff9f27
Amit Wani <mtwn105@gmail.com>
2022-02-25 20:52:07 +0530

    fixed redis url variable

--- redis.js
+++ redis.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = {
   connectToRedis: async () => {
     const client = createClient({
-      host: process.env.REDIS_URL,
+      url: process.env.REDIS_URL,
     });
 
     client.on("error", (err) => console.log("Redis Client Error", err));
